6aa29b1a89fa27edbaecc3ac861439224d418cbc
eduprats <eduprats@gmail.com>
2021-04-16 14:35:23 +0200

    added build script

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
 const esbuild = require('esbuild');
 const chokidar = require('chokidar');
+const isProduction = process.env.ELEVENTY_ENV === 'production';
 
 const buildJS = () => {
 	esbuild.buildSync({
 		entryPoints: ['src/main.js'],
 		bundle: true,
-		minify: false,
+		minify: isProduction,
 		sourcemap: false,
 		define: { DEV_MODE: true },
 		loader: { '.glsl': 'text', '.vert': 'text', '.frag': 'text' },
@@ -13,10 +14,12 @@ const buildJS = () => {
 	});
 }
 
-chokidar.watch('src/').on('change', (eventType, file) => {
-	console.log(`Updated JS [${eventType}]`);
-	buildJS();
-});
+if(!isProduction) {
+	chokidar.watch('src/').on('change', (eventType, file) => {
+		console.log(`Updated JS [${eventType}]`);
+		buildJS();
+	});
+}
 
 buildJS();
 
