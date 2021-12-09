eebae564f3ef049b6e83b2dfdd651e3c330da8ad
Edu Prats <eduprats@gmail.com>
2021-12-09 15:12:22 +0100

    Removed global canvas styling and added .view

--- src/js/core/App.ts
+++ src/js/core/App.ts
@@ -8,6 +8,7 @@ export class App extends WebGLSketch {
 		});
 		this.renderer.setClearColor(0xffffff, 1);
 		document.body.appendChild(this.domElement);
+		this.domElement.className = 'view';
 		
 		window.addEventListener('resize', (event)=>{
 			this.resize(window.innerWidth, window.innerHeight);
