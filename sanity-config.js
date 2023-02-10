3e778213bf36197e77736dd739c99e56a511fadc
Martí Fenosa <marti.fenosa@gmail.com>
2023-02-10 15:06:48 +0100

    Env ready

--- sanity-config.js
+++ sanity-config.js
@@ -1,7 +1,14 @@
-// Sanity credentials
-module.exports = {
+require('dotenv').config('../toolkitconfig.js');
+const sanityClient = require("@sanity/client");
+
+const config = {
   sanity: {
-    projectId: "nmkc2dgd",
-    dataset: "production",
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
+    apiVersion: process.env.SANITY_API_VERSION,
+    token: process.env.SANITY_TOKEN,
+    useCdn: false
   },
 };
+
+module.exports.client = sanityClient(config.sanity);
